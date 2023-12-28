3100b8436cde11937170351484414b07a051238f
punitha-96 <punithakathirvel6@gmail.com>
2023-12-28 15:56:10 +0530

    Rate Limiter and env added

---
+++ config/config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+};
+
+console.log(process.env.DB_USER);
+console.log(process.env.RATE_LIMIT_MAX);
