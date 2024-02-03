738644974e1b16325084d2becbbdf7341e23f26a
punitha-96 <punithakathirvel6@gmail.com>
2024-02-03 14:26:56 +0530

    code modified

--- queries.js
+++ queries.js
@@ -6,71 +6,63 @@ const pool = new Pool({
   password: "password",
   port: 5432,
 });
+const {
+  SELECT_SPECIFIC_COLUMNS,
+  SELECT_ROW_BY_ID,
+  INSERT_ROW,
+  UPDATE_ROW_BY_ID,
+  DELETE_ROW_BY_ID,
+} = require("./queryconstants");
+
 const getUsers = (request, response) => {
-  pool.query(
-    "SELECT name, email FROM users ORDER BY id ASC",
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      //console.log(results);
-      response.status(200).json(results.rows);
+  pool.query(SELECT_SPECIFIC_COLUMNS, (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(200).json(results.rows);
+  });
 };
 
 const getUserById = (request, response) => {
   const id = parseInt(request.params.id);
 
-  pool.query(
-    "SELECT name, email FROM users WHERE id = $1",
-    [id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).json(results.rows);
+  pool.query(SELECT_ROW_BY_ID, [id], (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(200).json(results.rows);
+  });
 };
 
 const createUser = (request, response) => {
   const { name, email } = request.body;
 
-  pool.query(
-    "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id",
-    [name, email],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      console.log(results);
-      const userId = results.rows[0].id;
-      response.status(201).send(`User added with ID: ${userId}`);
+  pool.query(INSERT_ROW, [name, email], (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    console.log(results);
+    const userId = results.rows[0].id;
+    response.status(201).send(`User added with ID: ${userId}`);
+  });
 };
 
 const updateUser = (request, response) => {
   const id = parseInt(request.params.id);
   const { name, email } = request.body;
 
-  pool.query(
-    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      response.status(200).send(`User modified with ID: ${id}`);
+  pool.query(UPDATE_ROW_BY_ID, [name, email, id], (error, results) => {
+    if (error) {
+      throw error;
     }
-  );
+    response.status(200).send(`User modified with ID: ${id}`);
+  });
 };
 
 const deleteUser = (request, response) => {
   const id = parseInt(request.params.id);
 
-  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+  pool.query(DELETE_ROW_BY_ID, [id], (error, results) => {
     if (error) {
       throw error;
     }
--- queryConstants.js
+++ queryConstants.js
@@ -1,13 +1,13 @@
-const SELECT_ALL_ROWS = "SELECT name, email FROM users ORDER BY id ASC";
-const SELECT_ROW_BY_ID = "SELECT name, email FROM users WHERE id = ?";
+const SELECT_SPECIFIC_COLUMNS =
+  "SELECT id, name, email FROM users ORDER BY id ASC";
+const SELECT_ROW_BY_ID = "SELECT name, email FROM users WHERE id = $1";
 const INSERT_ROW =
   "INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id";
-const UPDATE_ROW_BY_ID =
-  "UPDATE your_table_name SET column1 = ?, column2 = ?, column3 = ? WHERE id = ?";
-const DELETE_ROW_BY_ID = "DELETE FROM your_table_name WHERE id = ?";
+const UPDATE_ROW_BY_ID = "UPDATE users SET name = $1, email = $2 WHERE id = $3";
+const DELETE_ROW_BY_ID = "DELETE FROM users WHERE id = $1";
 
 module.exports = {
-  SELECT_ALL_ROWS,
+  SELECT_SPECIFIC_COLUMNS,
   SELECT_ROW_BY_ID,
   INSERT_ROW,
   UPDATE_ROW_BY_ID,
