918217fa4b91c0e899296114ff5caa0631feed4b
punitha-96 <punithakathirvel6@gmail.com>
2024-02-03 14:01:27 +0530

    code modified

--- queries.js
+++ queries.js
@@ -7,24 +7,31 @@ const pool = new Pool({
   port: 5432,
 });
 const getUsers = (request, response) => {
-  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
-    if (error) {
-      throw error;
+  pool.query(
+    "SELECT name, email FROM users ORDER BY id ASC",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      //console.log(results);
+      response.status(200).json(results.rows);
     }
-    //console.log(results);
-    response.status(200).json(results.rows);
-  });
+  );
 };
 
 const getUserById = (request, response) => {
   const id = parseInt(request.params.id);
 
-  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
-    if (error) {
-      throw error;
+  pool.query(
+    "SELECT name,email FROM users WHERE id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
     }
-    response.status(200).json(results.rows);
-  });
+  );
 };
 
 const createUser = (request, response) => {
